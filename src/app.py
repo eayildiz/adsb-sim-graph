0f2b5bba8309498ed9a3aae41a0656050fd5895b
Emir Alper Yildiz <eayildiz@outlook.com>
2024-02-12 14:02:20 +0300

    initial data manipulation structure commit

---
+++ src/app.py
@@ -0,0 +1,7 @@
+import dataManipulation
+
+def main():
+    data_man = dataManipulation()
+
+if(__name__ == "__main__"):
+    main()
---
+++ src/dataManipulation.py
@@ -0,0 +1,7 @@
+class dataManipulation:
+    def altitudeManipulation(altitude: float, changeRate: float):
+        return altitude * changeRate
+    def longtitudeManipulation(longtitude: float, changeRate: float):
+        return longtitude * changeRate
+    def heightManipulation(height: float, changeRate: float):
+        return height * changeRate
