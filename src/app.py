11f20ac63a7942fc5c99cbb8d7d941067637497f
Emir Alper Yildiz <eayildiz@outlook.com>
2024-02-26 22:39:02 +0300

    Data manipulation code improved based on data type and new data type added.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
-import dataManipulation
+import DataManipulation
 
 def main():
+    #Create build classes for every component.
+    #Create connection to API
+    #load data
     data_man = dataManipulation()
+    #load UI
+    #Create socket
+
 
 if(__name__ == "__main__"):
     main()
--- src/dataManipulation.py
+++ src/dataManipulation.py
@@ -1,7 +1,17 @@
+import DataTypes as DT
+
 class dataManipulation:
-    def altitudeManipulation(altitude: float, changeRate: float):
-        return altitude * changeRate
-    def longtitudeManipulation(longtitude: float, changeRate: float):
-        return longtitude * changeRate
-    def heightManipulation(height: float, changeRate: float):
-        return height * changeRate
+    def altitudeManipulation(planes: list[DT.Plane], altitude: float, changeRate: float):
+        for i in range(len(planes)):
+            planes[i].altitude = planes[i].altitude * changeRate
+        return planes
+    
+    def longtitudeManipulation(planes: list[DT.Plane], longtitude: float, changeRate: float):
+        for i in range(len(planes)):
+            planes[i].longtitude = planes[i].longtitude * changeRate
+        return planes
+    
+    def heightManipulation(planes: list[DT.Plane], height: float, changeRate: float):
+        for i in range(len(planes)):
+            planes[i].height = planes[i].height * changeRate
+        return planes
