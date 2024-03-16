94abc8cb2b8dcb5550cbcbdab869659940f44504
Emir Alper Yildiz <eayildiz@outlook.com>
2024-03-16 16:24:04 +0300

    fix problems of data manipulation function.

--- src/app.py
+++ src/app.py
@@ -4,9 +4,16 @@ import FlightFetcher as ff
 #get values from frontend
 def changePlaneDataByBounds(min_latitude, max_latitude, min_longtitude, max_longtitude, changeRate1, changeRate2, changeRate3):
     planes = ff.getFlightsByBounds(min_latitude, max_latitude, min_longtitude, max_longtitude)
-    DataManipulation.altitudeManipulation(planes, changeRate1)
-    DataManipulation.longtitudeManipulation(planes, changeRate2)
-    DataManipulation.latitudeManipulation(planes, changeRate3)
+    planes = DataManipulation.altitudeManipulation(planes, changeRate1)
+    planes = DataManipulation.longtitudeManipulation(planes, changeRate2)
+    planes = DataManipulation.latitudeManipulation(planes, changeRate3)
+
+def changePlaneDataByBounds(min_latitude, max_latitude, min_longtitude, max_longtitude, changeRate1, changeRate2, changeRate3):
+    planes = ff.getFlightsByBounds(min_latitude, max_latitude, min_longtitude, max_longtitude)
+    planes = DataManipulation.altitudeManipulation(planes, changeRate1)
+    planes = DataManipulation.longtitudeManipulation(planes, changeRate2)
+    planes = DataManipulation.latitudeManipulation(planes, changeRate3)
+
 
 def main():
     #Create build classes for every component.
